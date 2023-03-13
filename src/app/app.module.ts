f24d7fc0277e6ae496a6dfeedbdef7460066834d
Emmanuel Heredia <herediiaa.e@gmail.com>
2023-03-13 13:52:27 -0300

    navbar done

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SiteListComponent } from './site-list/site-list.component';
+import { LoginComponent } from './login/login.component';
+import { PasswordListComponent } from './password-list/password-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SiteListComponent,
+    LoginComponent,
+    PasswordListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'passwordmanager-f6a80',
+    appId: '1:690600791827:web:a3078fca4e7055abf394e6',
+    storageBucket: 'passwordmanager-f6a80.appspot.com',
+    apiKey: 'AIzaSyDmb0LSd85Tslwq3sHu7hnYFeoNK9smpFc',
+    authDomain: 'passwordmanager-f6a80.firebaseapp.com',
+    messagingSenderId: '690600791827',
+    measurementId: 'G-5D8157BTWN',
+  },
+    production: false
+}
