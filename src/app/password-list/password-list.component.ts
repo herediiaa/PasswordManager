d289733890243b739701a3de5ba31d8ac2a8fa93
Emmanuel Heredia <herediiaa.e@gmail.com>
2023-03-14 14:58:34 -0300

    transferring data between site-list and password-list done

--- src/app/password-list/password-list.component.ts
+++ src/app/password-list/password-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Site } from '../interfaces/sitesInfo.interface';
 @Component({
   selector: 'app-password-list',
   templateUrl: './password-list.component.html',
-  styleUrls: ['./password-list.component.css']
+  styleUrls: ['./password-list.component.css'],
 })
 export class PasswordListComponent {
-
+  siteInfo !: Site;
+  constructor(private readonly router: ActivatedRoute) {
+    this.router.queryParams.subscribe((data: any) => {
+      console.log(data);
+      this.siteInfo = data
+    });
+  }
 }
