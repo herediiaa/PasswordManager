03a3cc2acc7a96f819c4dfa940c0d3f24de877ec
Emmanuel Heredia <herediiaa.e@gmail.com>
2023-03-16 20:54:23 -0300

    basic login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  isError:boolean = false
+  constructor(private readonly authenticationService: AuthenticationService,
+    private router:Router) {}
+  onSubmit(value: any) {
+    this.authenticationService
+      .loadUser(value.email, value.password)
+      .then(() => {
+        console.log('User Acepted');
+        this.router.navigate(['/site-list'])
+      })
+      .catch((err) => {
+        this.isError = true;
+        setTimeout(() => {
+          this.isError = false
+        }, 2000);
+      });
+  }
 }
